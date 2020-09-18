5c221571e2bcc30d10bad204a99ff9d6436e47d0
viveksemwal <Viveksemwal1997@gmail.com>
2020-09-18 23:12:21 +0530

    dependency problem resolved

--- src/main/java/com/vivek/Ecommerce/ECommerceApplication.java
+++ src/main/java/com/vivek/Ecommerce/ECommerceApplication.java
@@ -3,11 +3,13 @@ package com.vivek.Ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
+
 	}
 
 }
